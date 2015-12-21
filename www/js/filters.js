c2fe1b1437dfd18cb7b4504052ade63a09b6bf57
KageKirin <kagekirin@gmail.com>
2015-12-21 15:36:00 +0900

    added 'encode' filter

--- www/js/filters.js
+++ www/js/filters.js
@@ -1,5 +1,12 @@
 angular.module('starter.filters', [])
 
+//uriComponent
+.filter('encode', function() {
+	return function(text) {
+		return encodeURIComponent(text);
+	};
+})
+
 .filter('decode', function() {
 	return function(encoded_text) {
 		return decodeURIComponent(encoded_text);
