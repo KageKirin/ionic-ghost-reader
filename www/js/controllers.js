176b1509d991f984fe4d2277d3c096f75002b6aa
KageKirin <kagekirin@gmail.com>
2015-12-21 11:15:56 +0900

    added controller and templates for post, tag, user views

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['starter.services'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -54,6 +54,65 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+/// blog controllers
+// list controllers
+.controller('BlogPostListCtrl', function($scope, BlogService) {
+	var response = BlogService.query(function() {
+		console.log(response);
+		$scope.posts = response.posts;
+	});
+})
+
+.controller('BlogTagListCtrl', function($scope, BlogService) {
+	var response = BlogService.query(function() {
+		console.log(response);
+		$scope.tags = response.tags;
+	});
+})
+
+.controller('BlogUserListCtrl', function($scope, BlogService) {
+	var response = BlogService.query(function() {
+		console.log(response);
+		$scope.users = response.users;
+	});
+})
+
+// single controllers
+.controller('BlogPostCtrl', function($scope, $stateParams, BlogService) {
+	console.log('BlogPostCtrl:', $stateParams);
+	var response = BlogService.get_post({postId: $stateParams.postId}, function() {
+		console.log('BlogPostCtrl.success -- response:', response);
+		$scope.post = response.posts[0];
+	},
+	function() {
+		console.log('BlogPostCtrl.failure -- response:', response, response.$promise);
+	});
+})
+
+.controller('BlogTagCtrl', function($scope, $stateParams, BlogService) {
+	console.log('BlogTagCtrl:', $stateParams);
+	var response = BlogService.get_tag({tagId: $stateParams.tagId}, function() {
+		console.log('BlogTagCtrl.success -- response:', response);
+		$scope.tag = response.tags[0];
+		$scope.posts = response.posts;
+	},
+	function() {
+		console.log('BlogTagCtrl.failure -- response:', response, response.$promise);
+	});
+})
+
+.controller('BlogUserCtrl', function($scope, $stateParams, BlogService) {
+	console.log('BlogUserCtrl:', $stateParams);
+	var response = BlogService.get_user({userId: $stateParams.userId}, function() {
+		console.log('BlogUserCtrl.success -- response:', response);
+		$scope.user = response.users[0];
+		$scope.posts = response.posts;
+	},
+	function() {
+		console.log('BlogUserCtrl.failure -- response:', response, response.$promise);
+	});
 })
 
 //semicolon must come last
