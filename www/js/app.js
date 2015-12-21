010bcc7ac4635572620c357a0ca3787775ccef52
KageKirin <kagekirin@gmail.com>
2015-12-21 11:08:52 +0900

    changed whitespace formatting

--- www/js/app.js
+++ www/js/app.js
@@ -4,70 +4,133 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+var starter = angular.module('starter', [
+	'ionic',
+	'ngSanitize',
+	'starter.controllers',
+])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+	$stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+		.state('app', {
+		url: '/app',
+		abstract: true,
+		templateUrl: 'templates/menu.html',
+		controller: 'AppCtrl'
+	})
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
+	.state('app.search', {
+		url: '/search',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/search.html'
+			}
+		}
+	})
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+	.state('app.browse', {
+		url: '/browse',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/browse.html'
+			}
+		}
+	})
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+	//playlist states -- obsolete
+	.state('app.playlists', {
+		url: '/playlists',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/playlists.html',
+				controller: 'PlaylistsCtrl'
+			}
+		}
+	})
+
+	.state('app.single', {
+		url: '/playlists/:playlistId',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/playlist.html',
+				controller: 'PlaylistCtrl'
+			}
+		}
+	})
+	//cover state
+	.state('app.cover', {
+		url: "/cover",
+		views: {
+				'menuContent': {
+					templateUrl: "templates/cover.html"
+			}
+		}
+	})
+
+
+	//tag states
+	.state('app.tags', {
+		url: "/tags",
+		views: {
+				'menuContent': {
+						templateUrl: "templates/tags.html",
+						controller: 'BlogTagListCtrl'
+				}
+		}
+	})
+
+	.state('app.tag', {
+		url: "/tags/:tagId",
+		views: {
+				'menuContent': {
+					templateUrl: "templates/tag.html",
+					controller: 'BlogTagCtrl'
+			}
+		}
+	})
+
+
+	//user states
+	.state('app.users', {
+		url: "/users",
+		views: {
+				'menuContent': {
+						templateUrl: "templates/users.html",
+						controller: 'BlogUserListCtrl'
+				}
+		}
+	})
+
+	.state('app.user', {
+		url: "/users/:userId",
+		views: {
+				'menuContent': {
+					templateUrl: "templates/user.html",
+					controller: 'BlogUserCtrl'
+			}
+		}
+	})
+
+
+	//closing semicolon follows
+	;
+	// if none of the above states are matched, use this as the fallback
+	$urlRouterProvider.otherwise('/app/playlists'); // obsolete
 });
