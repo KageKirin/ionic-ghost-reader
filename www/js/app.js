5d629b3341fcd55a80e565aff879c9eeb51b9064
KageKirin <kagekirin@gmail.com>
2015-12-29 10:39:14 +0900

    added ionic.io support added ionic-platform-web-client

--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,7 @@
 // 'starter.controllers' is found in controllers.js
 var starter = angular.module('starter', [
 	'ionic',
+	'ionic.service.core',
 	'ngSanitize',
 	'starter.endpoints',
 	'starter.globals',
@@ -17,7 +18,7 @@ var starter = angular.module('starter', [
 	$ionicPlatform.ready(function() {
 		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 		// for form inputs)
-		if (window.cordova && window.cordova.plugins.Keyboard) {
+		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
 			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 			cordova.plugins.Keyboard.disableScroll(true);
 
