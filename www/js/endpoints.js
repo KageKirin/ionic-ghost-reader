8280316098ae26d185a54200394d60805a48c5e8
KageKirin <kagekirin@gmail.com>
2015-12-29 10:32:54 +0900

    added current backend endpoint URL as comment

--- www/js/endpoints.js
+++ www/js/endpoints.js
@@ -1,11 +1,11 @@
 angular.module('starter.endpoints', ['ngResource', 'starter.services'])
 
 .constant('GhostApiEndpoint', {
-	url: 'http://localhost:8100/api'
+	url: 'http://localhost:8100/api'		//'https://kyokushindoapp.herokuapp.com/ghost/api/v0.1'
 })
 
 .constant('GhostBlogEndpoint', {
-	url: 'http://localhost:8100/api/blog'
+	url: 'http://localhost:8100/api/blog'	//'https://kyokushindoapp.herokuapp.com'
 })
 
 .constant('VideoEndpoint', {
